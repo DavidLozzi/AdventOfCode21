e830c9d1c1c95a12c8a00597c070a1ce410128bd
David Lozzi <lozzi.david@bcg.com>
2021-12-06 09:13:11 -0500

    animates day 6 laternfish

--- src/06-lanternfish/day6.js
+++ src/06-lanternfish/day6.js
@@ -1,10 +1,54 @@
 import React from 'react'
-import input1 from './input1';
+import input1 from './input1'
+import './fish.css'
 
 const Day6 = () => {
   const [puzzleInput, setPuzzleInput] = React.useState([]);
   const [first, setFirst] = React.useState()
+  const [fishList, setFishList] = React.useState([])
+  const [showFish, setShowFish] = React.useState(false)
+  const [day, setDay] = React.useState(0)
+  const [stopped, setStopped] = React.useState(false)
   const [second, setSecond] = React.useState()
+
+  const play = () => {
+    setShowFish(true);
+    setStopped(false)
+    const daysToRun = 80
+    let fishies = [...puzzleInput]
+    let days = 0
+
+    const loadFish = () => {
+      setTimeout(() => {
+        console.log('making fish')
+        const newFishes = []
+        fishies.forEach(fish => {
+          let newAge = fish - 1
+          if (newAge === -1) {
+            newFishes.push(6) // reset this fish
+            newFishes.push(8) // spawn a new fish
+          } else {
+            newFishes.push(newAge) // aging fish, push it in
+          }
+        })
+        setFishList(newFishes)
+        fishies = newFishes
+        days += 1
+        if (days < daysToRun && !stopped) {
+          setDay(days)
+          loadFish()
+        }
+      }, 500)
+    }
+
+    loadFish()
+  }
+
+  const clearFish = () => {
+    setStopped(true)
+    setShowFish(false)
+    setFishList([])
+  }
 
   // get the input into a usable array
   React.useEffect(() => {
@@ -61,6 +105,15 @@ const Day6 = () => {
       <h2>Day 6 - Lanternfish</h2>
       <h2>Answer #1</h2>
       {first}
+      <button className="smaller" onClick={play}>Play</button>
+      {showFish && <div>
+        {fishList.filter((f, i) => i < 10000).map((fish, index) => {
+          const randx = Math.random() * window.innerWidth
+          const randy = Math.random() * window.innerHeight
+          return <div className="fish" style={{ top: `${randx}px`, left: `${randy}px` }} key={`${fish}-${index}`}/>
+        })}
+        <div className="fish-counter">Day {day}<br /> {fishList.length} fishies<br /><a href="#stop" onClick={() => setStopped(true)}>stop</a> - <a href="#clear" onClick={clearFish}>clear</a></div>
+      </div>}
       <h2>Answer #2</h2>
       {second}
       <h3>Input: {puzzleInput.length} numbers</h3>
